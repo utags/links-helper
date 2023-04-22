5fe98824319a9c888e2ac78ac250deb7b28d8b82
pipecraft <pipecraft.net@gmail.com>
2023-04-22 17:36:54 +0800

    Add settings

--- src/content.ts
+++ src/content.ts
@@ -2,15 +2,38 @@ import {
   $$,
   addEventListener,
   getAttribute,
+  registerMenuCommand,
   setAttribute,
 } from "browser-extension-utils"
 import type { PlasmoCSConfig } from "plasmo"
 
+import {
+  getSettingsValue,
+  initSettings,
+  showSettings,
+} from "./components/settings"
+
 const origin = location.origin
+const host = location.host
 
 export const config: PlasmoCSConfig = {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   run_at: "document_end",
+}
+
+const settingsTable = {
+  enable: {
+    title: "Enable",
+    defaultValue: true,
+  },
+  [`enableCurrentSite_${host}`]: {
+    title: "Enable current site",
+    defaultValue: true,
+  },
+}
+
+function registerMenuCommands() {
+  registerMenuCommand("⚙️ 设置", showSettings, "o")
 }
 
 const addAttribute = (element: HTMLElement, name: string, value: string) => {
@@ -38,6 +61,12 @@ const shouldOpenInNewTab = (url: string | undefined) => {
 }
 
 const setAttributeAsOpenInNewTab = (element: HTMLElement) => {
+  // console.log(element)
+  // console.log(element.getAttribute("href"))
+  // console.log(element.href)
+  // console.log(element.hostname)
+  // console.log(element.origin)
+  // console.log(element.pathname)
   const href = element.href as string | undefined
   if (shouldOpenInNewTab(href)) {
     setAttribute(element, "target", "_blank")
@@ -45,19 +74,42 @@ const setAttributeAsOpenInNewTab = (element: HTMLElement) => {
   }
 }
 
-function main() {
+async function main() {
+  await initSettings({
+    title: "🔗 Links Helper",
+    footer: `
+    <p>Reload the page to take effect</p>
+    <p>
+    <a href="https://github.com/utags/links-helper/issues" target="_blank">
+    Report and Issue...
+    </a></p>
+    <p>Made with ❤️ by
+    <a href="https://www.pipecraft.net/" target="_blank">
+      Pipecraft
+    </a></p>`,
+    settingsTable,
+  })
+  registerMenuCommands()
+
+  if (
+    !getSettingsValue("enable") ||
+    !getSettingsValue(`enableThisSite_${host}`)
+  ) {
+    return
+  }
+
   addEventListener(
     document,
     "click",
     (event) => {
-      let linkElement = event.target as HTMLElement | undefined
+      let anchorElement = event.target as HTMLElement | undefined
 
-      while (linkElement && linkElement.tagName !== "A") {
-        linkElement = linkElement.parentNode as HTMLElement | undefined
+      while (anchorElement && anchorElement.tagName !== "A") {
+        anchorElement = anchorElement.parentNode as HTMLElement | undefined
       }
 
-      if (linkElement) {
-        setAttributeAsOpenInNewTab(linkElement)
+      if (anchorElement) {
+        setAttributeAsOpenInNewTab(anchorElement)
       }
     },
     true
@@ -68,4 +120,5 @@ function main() {
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises, unicorn/prefer-top-level-await
 main()
